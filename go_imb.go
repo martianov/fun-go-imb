8d8953e77003260a4842b648b094f3fc7e370da0
Andrey Martianov <andrey.martianov@xored.com>
2015-08-30 19:50:36 +0600

    config file and static resources server

--- go_imb.go
+++ go_imb.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"os"
+	"io/ioutil"
 	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
+	"github.com/gorilla/mux"
 )
+
+type GoImbConfiguration struct {
+		Port int
+		Webapp string
+}
+
+func readConfiguration() GoImbConfiguration {
+	confFilePath := os.Getenv("GOIMBCONF")
+	if len(confFilePath) == 0  {
+		confFilePath = "src/github.com/martianov/go-imb/go-imb.conf.default"
+	}
+	configFileData, readErr := ioutil.ReadFile(confFilePath)
+	if readErr != nil {
+        panic(fmt.Errorf("Failed to read configuration file %s: %v", confFilePath, readErr))
+    }
+
+	configuration := GoImbConfiguration{}
+	parseErr := yaml.Unmarshal([]byte(configFileData), &configuration)
+	if parseErr != nil {
+		panic(fmt.Errorf("Failed to parse configuration file %s: %v", confFilePath, parseErr))
+	}
+
+	fmt.Printf("Configuration file: %s\n", confFilePath)
+
+    return configuration
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	configuration := readConfiguration()
+
+	// handle all requests by serving a file of the same name
+	fs := http.Dir(configuration.Webapp)
+	fileHandler := http.FileServer(fs)
+
+	// setup routes
+	router := mux.NewRouter()
+
+	router.Handle("/", http.RedirectHandler("/webapp/", 302))
+	router.PathPrefix("/webapp/").Handler(http.StripPrefix("/webapp", fileHandler))
+	http.Handle("/", router)
+
+	http.ListenAndServe(fmt.Sprintf(":%v", configuration.Port), nil);
 }
